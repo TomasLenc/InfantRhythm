803c26ac3ce13949c0e5cd37adaa1fd72fc8aa47
Tomas Lenc <tomas.lenc@gmail.com>
2022-04-29 15:42:33 +0200

    paths check in R

--- main.R
+++ main.R
@@ -1,11 +1,16 @@
 rm(list=ls())
 
-reports_path <- '/datadisk/Dropbox/projects/XPInfant/derivatives/reports'
+# ============== set this to your dataset root path ============== 
+experiment_path <- '/datadisk/Dropbox/projects/XPInfant'
+# ================================================================
+
+reports_path <- file.path(experiment_path, 'derivatives/reports')
+if (!dir.exists(reports_path)){
+    dir.create(reports_path, recursive=TRUE)    
+}
 
 roi <- 'front'
-rmarkdown::render('report_lmer.Rmd', output_file=file.path(reports_path, sprintf('roi-%s_reportLME.html',roi)))
 rmarkdown::render('report_anova.Rmd', output_file=file.path(reports_path, sprintf('roi-%s_reportANOVA.html',roi)))
 
 roi <- 'best'
-rmarkdown::render('report_lmer.Rmd', output_file=file.path(reports_path, sprintf('roi-%s_reportLME.html',roi)))
 rmarkdown::render('report_anova.Rmd', output_file=file.path(reports_path, sprintf('roi-%s_reportANOVA.html',roi)))
